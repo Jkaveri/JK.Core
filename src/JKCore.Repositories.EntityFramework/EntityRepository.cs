bbc26677f09c6ed0e73cffc51aa0513ae159b541
Henry Nguyen <haiho1992@gmail.com>
2016-12-31 15:05:38 +0700

    Adjust namespace JK.Core to JKCore

--- src/JKCore.Repositories.EntityFramework/EntityRepository.cs
+++ src/JKCore.Repositories.EntityFramework/EntityRepository.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Ho Nguyen. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-namespace JK.Core.Repositories.EntityFramework
+namespace JKCore.Repositories.EntityFramework
 {
     #region
 
