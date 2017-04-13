44bbf02fbcd8629ccdc0ea858ae17b7093f8d147
Henry Nguyen <haiho1992@gmail.com>
2017-04-13 19:49:28 +0700

    Add JKCore service extensions.

---
+++ src/JKCore/JKCoreServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JKCore
+{
+    using JKCore.Validators;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class JKCoreServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJKCore(this IServiceCollection services)
+        {
+            services.AddTransient<AnnotationsValidator>();
+
+            return services;
+        }
+    }
+}
